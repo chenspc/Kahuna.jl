17f34590fe4609e6d7f3fd14e3f9c9f23f5695f4
C Huang <chen1huang2@gmail.com>
2019-11-12 20:02:59 +0000

    added SafeTestSets and updated Project.toml to include [compat]

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
 using Owen
 using Test
+using SafeTestsets
 using Libdl
 
-push!(Libdl.DL_LOAD_PATH, "/usr/lib") 
+push!(Libdl.DL_LOAD_PATH, "/usr/lib")
 
-include("test_fileio.jl")
-@testset "Owen.jl" begin
-    # Write your own tests here.
-end
+@safetestset "File I/O" begin include("test_fileio.jl") end
